6a513d52ab426162e64d67f06d01a752e531b061
Efu <o@efu.me>
2024-04-07 12:17:49 +0800

    init first

---
+++ sponsorkit.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  includePrivate: true,
+  tiers: [
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    {
+      title: 'Backers',
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: presets.medium,
+      composeAfter: (composer, _tierSponsors, _config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
+})
