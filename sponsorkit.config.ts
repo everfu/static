0490a8cb6298ee511a4e62e4842e6670e1429ca6
Efu <o@efu.me>
2024-05-16 12:14:03 +0800

    chore: update setting

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,22 +1,40 @@
-import { defineConfig, presets } from 'sponsorkit'
+import { BadgePreset, defineConfig, presets } from 'sponsorkit'
+import fs from 'fs/promises'
+
+const past: BadgePreset = {
+  avatar: {
+    size: 20,
+  },
+  boxWidth: 22,
+  boxHeight: 22,
+  container: {
+    sidePadding: 35,
+  },
+}
 
 export default defineConfig({
-  includePrivate: true,
   tiers: [
     {
       title: 'Past Sponsors',
       monthlyDollars: -1,
-      preset: presets.xs,
+      preset: past,
     },
     {
       title: 'Backers',
+      preset: presets.small,
     },
     {
       title: 'Sponsors',
       monthlyDollars: 10,
-      preset: presets.medium,
-      composeAfter: (composer, _tierSponsors, _config) => {
-        composer.addSpan(10)
+      preset: {
+        avatar: {
+          size: 42,
+        },
+        boxWidth: 52,
+        boxHeight: 52,
+        container: {
+          sidePadding: 30,
+        },
       },
     },
     {
@@ -33,6 +51,80 @@ export default defineConfig({
       title: 'Platinum Sponsors',
       monthlyDollars: 500,
       preset: presets.xl,
-    }
+    },
   ],
+
+  sponsorsAutoMerge: true,
+
+  mergeSponsors: [
+    [
+      { login: 'patak-dev', provider: 'github' },
+      { login: 'patak', provider: 'opencollective' },
+    ],
+    [
+      { login: 'serkodev', provider: 'github' },
+      { login: 'serko', provider: 'opencollective' },
+    ],
+    [
+      { login: 'WebWorkerTech', provider: 'github' },
+      { login: 'web-worker-podcast', provider: 'opencollective' },
+    ],
+    [
+      { login: 'kissu', provider: 'github' },
+      { login: 'kissu', provider: 'opencollective' },
+    ],
+  ],
+
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      'sponsors.json',
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== 'PRIVATE')
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === 'Organization'
+            }
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    )
+  },
+
+  outputDir: '.',
+  formats: ['svg', 'png'],
+
+  renders: [
+    {
+      name: 'sponsors.part1',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars >= 9.9
+    },
+    {
+      name: 'sponsors.part2',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars < 9.9 && sponsor.monthlyDollars >= 0
+    },
+    {
+      name: 'sponsors.past',
+      width: 800,
+      filter: (sponsor) => sponsor.monthlyDollars < 0
+    },
+    {
+      name: 'sponsors.circles',
+      width: 1000,
+      includePastSponsors: true,
+      renderer: 'circles',
+      circles: {
+        radiusPast: 3
+      }
+    }
+  ]
 })
