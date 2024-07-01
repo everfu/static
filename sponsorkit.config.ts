7260636643d845e7e3d22c3231786b92c19e5183
Ever Fu <o@efu.me>
2024-07-02 01:14:27 +0800

    feat: 增加赞赏统计文件生成

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -75,29 +75,6 @@ export default defineConfig({
     ],
   ],
 
-  async onSponsorsReady(sponsors) {
-    await fs.writeFile(
-      'sponsors.json',
-      JSON.stringify(
-        sponsors
-          .filter((i) => i.privacyLevel !== 'PRIVATE')
-          .map((i) => {
-            return {
-              name: i.sponsor.name,
-              login: i.sponsor.login,
-              avatar: i.sponsor.avatarUrl,
-              amount: i.monthlyDollars,
-              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
-              org: i.sponsor.type === 'Organization'
-            }
-          })
-          .sort((a, b) => b.amount - a.amount),
-        null,
-        2
-      )
-    )
-  },
-
   outputDir: '.',
   formats: ['svg', 'png'],
 
@@ -133,6 +110,28 @@ export default defineConfig({
       circles: {
         radiusPast: 3
       }
-    }
-  ]
+    },
+  ],
+  async onSponsorsReady(sponsors) {
+    await fs.writeFile(
+      'sponsors.json',
+      JSON.stringify(
+        sponsors
+          .filter((i) => i.privacyLevel !== 'PRIVATE')
+          .map((i) => {
+            return {
+              name: i.sponsor.name,
+              login: i.sponsor.login,
+              avatar: i.sponsor.avatarUrl,
+              amount: i.monthlyDollars,
+              link: i.sponsor.linkUrl || i.sponsor.websiteUrl,
+              org: i.sponsor.type === 'Organization'
+            }
+          })
+          .sort((a, b) => b.amount - a.amount),
+        null,
+        2
+      )
+    )
+  },
 })
